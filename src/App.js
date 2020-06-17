da0bd8453383b8697655ff346bf617e07336f784
Gray754 <lgc9312@gmail.com>
2020-06-16 21:08:46 -0500

    added functions to login and logout of the app using firebase auth

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import firebase, { auth, provider } from './firebase'
+
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [user, setUser] = React.useState(null)
+
+	React.useEffect(() => {
+		auth.onAuthStateChanged((user) => {
+			if (user) {
+				setUser({
+					name: user.displayName,
+					photo: user.photoURL,
+					id: user.uid,
+				})
+			}
+		})
+	}, [])
+
+	const loginUser = () => {
+		auth.signInWithPopup(provider).then((result) => {
+			const user = result.user
+			console.log(user)
+			setUser({
+				name: user.displayName,
+				photoURL: user.photoURL,
+				id: user.uid,
+			})
+		})
+	}
+
+	const logoutUser = () => {
+		auth.signOut().then(() => {
+			setUser(null)
+		})
+	}
+
+	let signInButton = <button onClick={loginUser}>Sign In</button>
+	let userImg
+	if (user) {
+		signInButton = <button onClick={logoutUser}>Sign Out</button>
+		userImg = (
+			<img
+				style={{ borderRadius: '50%', border: '1px solid blue' }}
+				width='200px'
+				src={user.photo}
+			/>
+		)
+	}
+
+	console.log(user)
+
+	return (
+		<>
+			{user ? <h3>Welcome {user.name}</h3> : <h3>This is a firebase app</h3>}
+			{userImg}
+			{signInButton}
+		</>
+	)
 }
 
-export default App;
+export default App
